a7f3b1808f9aa884f93636725dc2bce29b7e094e
Bohdan <bogmira777@gmail.com>
2023-06-24 22:59:38 +0200

    Recompiled styles

--- wp-content/themes/avrora/includes/setup.php
+++ wp-content/themes/avrora/includes/setup.php
@@ -4,9 +4,9 @@ function avrora_setup_theme()
     add_theme_support('post-thumbnails');
     register_nav_menu('primary', __('Primary Menu', 'avrora'));
     add_theme_support('custom-logo', [
-        'height'      => 100,
-        'width'       => 400,
+        'height' => 100,
+        'width' => 400,
         'flex-height' => true,
-        'flex-width'  => true,
+        'flex-width' => true,
     ]);
 }
